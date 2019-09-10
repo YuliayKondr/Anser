444542aac43074451cfe6559fbc4c666151847ef
YuliayKondr <juliakondr17@gmail.com>
2019-09-10 14:01:53 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/AnserBall.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace WpfApp1
+{
+    public class AnserBall:INotifyPropertyChanged
+    {
+        private static string[] massAnser = new string[]
+            {
+                "Бесспорно",
+                "Предрешено",
+                "Никаких сомнений",
+                "Определённо да",
+                "Можешь быть уверен \nв этом",
+                "Мне кажется — «да»",
+                "Вероятнее всего",
+                "Хорошие перспективы",
+                "Знаки говорят — «да»",
+                "Да",
+                "Пока не ясно, \nпопробуй снова",
+                "Спроси позже",
+                "Лучше не рассказывать",
+                "Сейчас нельзя предсказать",
+                "Сконцентрируйся \nи спроси опять",
+                "Даже не думай",
+                "Мой ответ — «нет»",
+                "По моим данным — «нет»",
+                "Перспективы не \nочень хорошие",
+                "Весьма сомнительно"
+        };
+        public static string Anser()
+        {
+            Random random = new Random();
+            return massAnser[random.Next(0, 20)];
+        }
+        private string _privet;
+        public string Privet
+        {
+            get { return _privet; }
+            set { _privet = value; }
+        }
+        public string Durish()
+        {
+            return "Ты меня дуришь";
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+             
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            if (!myBall.Resources.Contains("anser")) myBall.Resources.Add("anser", "Задай вопрос");
+            else
+            {
+                myBall.Resources["anser"] = "Задай вопрос";
+            }
+        }
+      
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {            
+            if (!myBall.Resources.Contains("anser")) myBall.Resources.Add("anser", AnserBall.Anser());
+            else
+            {
+                myBall.Resources["anser"]=AnserBall.Anser();
+            }
+           
+        }
+
+        private void MyBall_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
